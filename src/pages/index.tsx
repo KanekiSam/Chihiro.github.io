a62ea08cd31ee24f8d5a3f5479c068c62ca1da50
KanekiSam <847106531@qq.com>
2020-04-06 01:10:14 +0800

    add

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import styles from './index.less';
+import { user } from '@/data/user';
 
 export default () => {
+  const starRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    // setInterval(() => {
+    for (let item = 1; item < 200; item++) {
+      const star = document.createElement('div');
+      star.className = styles.stars;
+      star.style.left = Math.random() * window.innerWidth + 'px';
+      star.style.top = Math.random() * window.innerHeight + 'px';
+      starRef.current?.appendChild(star);
+    }
+    // }, 100);
+  }, []);
   return (
-    <div>
-      <h1 className={styles.title}>Page index</h1>
+    <div className={styles.homeModule}>
+      <div className={styles.starModule} ref={starRef}></div>
+      <div className={styles.center}>
+        <div className={styles.userName}>{user.name}</div>
+        <div className={styles.avatar}>
+          <img src={require('../static/avatar.png')} alt="" />
+        </div>
+        <div className={styles.describe}>{user.describe}</div>
+      </div>
     </div>
   );
-}
+};
